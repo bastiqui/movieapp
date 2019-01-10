1cc55c66dda3468f30f616e145d9b4ca0cb9373f
bastiqui <bastiqui@gmail.com>
2019-01-10 14:06:25 +0100

    GestureDetector implemented and removed

--- app/src/main/java/com/example/bastiqui/moviesapp/activities/MyZoneActivity.java
+++ app/src/main/java/com/example/bastiqui/moviesapp/activities/MyZoneActivity.java
@@ -2,13 +2,13 @@ package com.example.bastiqui.moviesapp.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
